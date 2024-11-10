5efc0fb9deb3ecbeb7cad0910999da072fdb43d2
UseK <use.k.james@gmail.com>
2024-11-10 11:47:38 +0900

    Start to implement Board and Mino

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Board {
+    inner: Vec<Vec<bool>>,
+}
+
+impl Board {
+    fn new(width: usize, height: usize) -> Self {
+        Board {
+            inner: vec![vec![false; width]; height],
+        }
+    }
+}
+
+struct Mino {
+    inner: Vec<Vec<bool>>,
+}
+
+impl Mino {
+    fn from_str(s: &str) -> Self {
+        Self {
+            inner: s
+                .lines()
+                .filter(|l| !l.is_empty())
+                .map(|l| l.chars().map(|c| c == '#').collect())
+                .collect(),
+        }
+    }
+}
+
+const A: &str = "
+###
+.##
+";
+
+const B: &str = "11";
+
+#[test]
+fn test_mino_from_str() {
+    let mino = Mino::from_str(A);
+    assert_eq!(
+        mino.inner,
+        vec![vec![true, true, true], vec![false, true, true]]
+    )
+}
