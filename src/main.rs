fde306b4c198266cd79da06bf5364a9b0aca8de7
UseK <use.k.james@gmail.com>
2025-06-01 15:05:01 +0900

    Fix typo package name 'slover' to 'solver'

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use rayon::ThreadPoolBuilder;
 use std::env;
-use tiling_mino_slover::{Board, Mino, check_wall_count};
+use tiling_mino_solver::{Board, Mino, check_wall_count};
 
 pub const NUM_THREADS: usize = 8;
 
